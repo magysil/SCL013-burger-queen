34f77f1ac44e5ae3400edadf30373a3e43aff842
Magally <magallysilvab@gmail.com>
2020-07-06 21:37:35 -0400

    Eliminado H3, Crear Boton

--- burger/src/Component/Meseros/index.js
+++ burger/src/Component/Meseros/index.js
@@ -4,20 +4,20 @@ import React, { Component } from "react";
 import "../Global/Css/Meseros.css";
 import Itemenu from "../ItemMenu/itemMenu";
 import Executingorder from "../ItemMenu/Executingorder";
+import Total from "../ItemMenu/Total";
 
 class Meseros extends Component {
   render() {
     return (
       <div className="Meseros">
       <div className="Menus">
-        <h2>Menus</h2>
-        <h3>Yo soy el contenedor de menus</h3>
+        <h2>Menus del Día</h2>
          <Itemenu />
         </div>
         <div className="Agregar">
-        <h3>Yo tomo los pedidos</h3>
           <Executingorder />
-        <h3>Enviar</h3>
+          <Total />
+        <button className='btn btn-danger'>Enviar</button>
       </div>
       </div>
     );
