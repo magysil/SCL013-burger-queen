a97efb808e8aa3749ca09edcd1ffb9d1507c825e
“ivvnv” <ivana.rivera.c@gmail.com>
2020-07-07 12:17:20 -0400

    quitar li y dejar solo botones con modal

--- burger/src/Component/ItemMenu/itemMenu.js
+++ burger/src/Component/ItemMenu/itemMenu.js
@@ -51,26 +51,24 @@ class Itemenu extends Component {
           </button>
         </div>
         <div className="container w-75">
-          <ul className="list-group">
+          
             {data.filter(item => item.type ===`${mealt}`).map(filtertype => (
               
-              <li onClick={e => this.handleClick(e, filtertype)} key={filtertype.objectID} 
-              className="list-group-item d-flex justify-content-between align-items-center">
-              <ButtonToolbar>
-                <Button
-                  variant = 'primary'
-                  onClick={() => this.setState({addModalShow:true})}
-                  >{filtertype.name}</Button>
+              <>
+              <Button onClick={e => this.handleClick(e, filtertype)} key={filtertype.objectID} 
+              type="button" className="btn btn-light custom" variant = 'primary' onClick={() => this.setState({addModalShow:true})}>
+              
+                {filtertype.name}</Button>
 
-                  <OptionsModal 
+                  <OptionsModal
                   show={this.state.addModalShow}
                   onHide={addModalClose} />
-                </ButtonToolbar> 
+                
               <span className="badge badge-primary badge-pill">{filtertype.price}</span>
-              </li>
+              </>
 
             ))}
-          </ul>
+          
         </div>
       </div>
     );
