39f99adb204fd89428a5e3918d80d0af0cca2904
Magally <magallysilvab@gmail.com>
2020-07-07 21:33:01 -0400

    Agregar y eliminar items, conexion modulos

--- burger/src/Component/Meseros/index.js
+++ burger/src/Component/Meseros/index.js
@@ -6,15 +6,39 @@ import Itemenu from "../ItemMenu/itemMenu";
 import Executingorder from "../ItemMenu/Executingorder";
 
 class Meseros extends Component {
+
+  state = {
+    order: []
+  };
+
+  // Agregar items al pedido
+  addItem(item) {
+    this.setState(e => ({
+      order: [...e.order, item]
+    }));
+  } 
+
+   // Elimina item del pedido
+   deleteItem = i => {
+    let order = [...this.state.order];
+    order.splice(i, 1);
+    this.setState({
+      order: order
+    });
+  };
+
   render() {
+    console.log(this.state.order);
     return (
       <div className="Meseros">
       <div className="Menus">
         <h2>Menus del Día</h2>
-         <Itemenu />
+         <Itemenu addItem={this.addItem.bind(this)}/>
         </div>
         <div className="Agregar">
-          <Executingorder />
+          <Executingorder 
+          total={this.state.order}
+          deleteItem={this.deleteItem.bind(this)} />
         <button className='btn btn-danger'>Enviar</button>
       </div>
       </div>
