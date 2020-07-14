b4bd360fb693e8045f181fc1385562d0042aa5d2
Magally <magallysilvab@gmail.com>
2020-07-14 10:22:01 -0400

    arreglos merge

--- burger/src/Component/Meseros/index.js
+++ burger/src/Component/Meseros/index.js
@@ -5,7 +5,7 @@ import Itemenu from "../ItemMenu/itemMenu";
 import Executingorder from "../ItemMenu/Executingorder";
 import InputContainer from "../ItemMenu/InputContainer";
 import Total from "../ItemMenu/Total";
-import db from "../../ConfigDB/firebase"
+import db from "../../configDB/firebase"
 
 class Meseros extends Component {
 // Parte vacio
@@ -102,7 +102,7 @@ numTable(tableNumber) {
           deleteItem={this.deleteItem.bind(this)} />
 
           <Total
-          total={total}/>
+          total={total}
           sendOrder={this.sendOrder.bind(this)}/>
 
           </div>
