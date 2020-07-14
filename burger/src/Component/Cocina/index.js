14921e9183e4a277eebfa4880567231e46fd2242
“ivvnv” <ivana.rivera.c@gmail.com>
2020-07-14 14:32:15 -0400

    muestra pedidos desde firebase en cocina

--- burger/src/Component/Cocina/index.js
+++ burger/src/Component/Cocina/index.js
@@ -1,16 +1,55 @@
+/* eslint-disable no-unused-expressions */
+
 // Dependencies
 import React, { Component } from "react";
 import "../Global/Css/Cocina.css";
 
+import db from '../../configDB/firebase';
+
 class Cocina extends Component {
+  state = {
+    orders: [],
+  };
+
+  componentDidMount() {
+    // console.log('mounted')
+    db.collection("orders")
+      .get()
+      .then((snapshot) => {
+        const orders = [];
+        snapshot.forEach((doc) => {
+          const data = doc.data();
+          orders.push(data);
+        });
+        this.setState({ orders: orders });
+        // console.log(orders)
+      })
+      .catch((error) => console.log(error));
+  }
+
   render() {
     return (
       <div className="Cocina">
         <div className="Pedidos">
-        <h2>Pedidos</h2>
-        <h3>Contenedor de pedidos</h3>
+          <h2>Pedidos</h2>
+          {this.state.orders &&
+            this.state.orders.map( comanda => {
+              return (
+                <div>
+                  <p>{comanda.nfactura}</p>
+                  <p>Mesa {comanda.table}</p>
+                  <p>
+                    Pedido:{" "}
+                    {comanda.order.map((pedido) => {
+                      return pedido.name;
+                    })}
+                  </p>
+                  <p>Estado: {comanda.status}</p>
+                </div>
+              );
+            })}
         </div>
-        </div>
+      </div>
     );
   }
 }
