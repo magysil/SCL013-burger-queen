0c30033bcb5ad1a0442e19e843efe66fb5fbc72d
Magally <magallysilvab@gmail.com>
2020-07-19 20:49:59 -0400

    version finalized - complete

---
+++ public/precache-manifest.cd6f57a5eeb4defff9812f035551ba93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b84dd09fbfd4ca7b84052454cf934c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d0d5ee0d765c6d1c911",
+    "url": "/static/css/main.f0f959e3.chunk.css"
+  },
+  {
+    "revision": "f61c517938c9beab73af",
+    "url": "/static/js/2.5af82346.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.5af82346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0d5ee0d765c6d1c911",
+    "url": "/static/js/main.d14ad674.chunk.js"
+  },
+  {
+    "revision": "f7bd350b4e661202d4b7",
+    "url": "/static/js/runtime-main.b27aebe0.js"
+  },
+  {
+    "revision": "caa30bcb78bbe8c5bc016625b686af8d",
+    "url": "/static/media/crown.caa30bcb.svg"
+  },
+  {
+    "revision": "f69cefa47dcb526e0ee19458c9d7ed47",
+    "url": "/static/media/logoBQ.f69cefa4.svg"
+  }
+]);
